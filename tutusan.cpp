e525107c78339cd220457596aa58c096911659df
tutu_ubuntu1 <tumuri4071.dev@gmail.com>
2022-11-01 22:53:05 +0900

    change tutusan.cpp ss 12-re01

--- tutusan.cpp
+++ tutusan.cpp
@@ -2,6 +2,30 @@
 #include <functional> //これいる
 
 int main(){
-    auto func1 = [](int i, int j){std::cout << (i + j) << std::endl;};
-    func1(99, 1); //100
+    int keisan;
+    
+    auto func1 = [](int i, int j) {
+        std::cout << (i + j);
+        return i + j;
+    };
+    auto func2 = [](double i, double j) -> int {
+        std::cout << (i + j) << std::endl;
+        return i + j;
+    };
+    auto func3 = [] (auto i ,auto j) {
+        std::cout << (i + j) << std::endl;
+        return i + j;
+    }; 
+    //こんなんで許されるのか？
+
+    func1(1, 2);
+    keisan = func2(1.35, 2.75);
+    func3(1, 1.41);
+
+    std::cout << keisan << std::endl;
+
+    return 0;
+
+    //  結果:許された
+
 }
